4a67c953a34d95dd5210ba83291e80b3aee3333c
Luis Fernando <86975316+LuiFeAn@users.noreply.github.com>
2021-09-27 16:28:19 -0300

    1.0.1

---
+++ app/json.js
@@ -0,0 +1,6 @@
+//Pseudo-Json
+let games = [
+    {game: "The last of US",img: "https://gmedia.playstation.com/is/image/SIEPDC/the-last-of-us-remastered-pack-01-ps4-en-23jul20?$native$"},
+    {game: "Dark Souls",img: "https://image.api.playstation.com/gs2-sec/appkgo/prod/CUSA03365_00/2/i_1bf8d3317ff642710196c85c859320f0a9345e3c65a9132d588239fab877d81a/i/icon0.png"},
+    {game: "Cuphead",img: "https://image.api.playstation.com/gs2-sec/appkgo/prod/CUSA20469_00/1/i_3fa25ceb0c4d77fb214dce8cd1ad03d339d78d5db47c4f2511260595cbb38d00/i/icon0.png"},
+]
